c917b28dce36eba35c6cd25513bffb7a1dc2fe2c
miguel <miguel.mail@zoho.com>
2012-11-05 16:11:19 +0100

    file descriptor

--- stlviewer.py
+++ stlviewer.py
@@ -2,17 +2,18 @@ import bpy
 import glob
 from io_mesh_stl import stl_utils
 from io_mesh_stl import blender_utils
+from bpy.props import StringProperty, BoolProperty, CollectionProperty
+from bpy_extras.io_utils import ExportHelper, ImportHelper
 
 
 #path = "/home/sio2/projectroom/signature_vases/code/repositorie/files/*.stl"
-path = "/yourpath/*.stl"
+path = ""
 
 fileidx = 0
 filelist = glob.glob(path)
 
 print(glob.glob(path))
 print(filelist)
-
 
 class GeneralUI(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
@@ -24,6 +25,11 @@ class GeneralUI(bpy.types.Panel):
         layout = self.layout
         obj = context.object
         scene = context.scene
+        
+        col = layout.column(align=True)
+        row = col.row()
+        row.operator("open.buttom", text="open")        
+
         col = layout.column(align=True)
         row = col.row()
         row.operator("next.buttom", text="next")        
@@ -44,13 +50,16 @@ class NextButtom(bpy.types.Operator):
 
     def execute(self, context):
         global fileidx
+        global filelist
 
         #deletes all the objects
         select_all_objects()
         bpy.ops.object.delete()
 
         #next object
-        print(filelist)
+        print("-- Filename opened:")
+        print("idx"+str(fileidx))
+        print(filelist[fileidx])
 
         faces,verts = stl_utils.read_stl(filelist[fileidx])
         filename = filelist[fileidx]
@@ -78,6 +87,8 @@ class PrevButtom(bpy.types.Operator):
         bpy.ops.object.delete()
 
         #next object
+        print("-- Filename opened:")
+        print("idx:"+str(fileidx))
         print(filelist[fileidx])
 
         faces,verts = stl_utils.read_stl(filelist[fileidx])
@@ -86,6 +97,42 @@ class PrevButtom(bpy.types.Operator):
         bpy.data.objects[find_object(filename[len(filename)-14 : len(filename)])].select = True
         bpy.context.scene.objects.active = bpy.data.objects[find_object(filename[len(filename)-14 : len(filename)])]
         fileidx = fileidx - 1
+        return {'FINISHED'}
+
+
+class OpenButtom(bpy.types.Operator,ImportHelper):
+    '''Load STL triangle mesh data'''
+    bl_idname = "open.buttom"
+    bl_label = "Import STL"
+    bl_options = {'UNDO'}
+
+    filename_ext = ".stl"
+
+    directory = StringProperty(
+            subtype='DIR_PATH',
+            )
+    
+    def execute(self, context):
+        global path
+        global filelist
+        global fileidx
+
+        path = self.directory+"*.stl"
+        filelist = glob.glob(path)
+        
+        #loading the first object
+        #deletes all the objects
+        select_all_objects()
+        bpy.ops.object.delete()
+
+        faces,verts = stl_utils.read_stl(filelist[fileidx])
+        filename = filelist[fileidx]
+        ob1 = createMesh( filename[len(filename)-14 : len(filename)], verts, [], faces)
+        bpy.data.objects[find_object(filename[len(filename)-14 : len(filename)])].select = True
+        bpy.context.scene.objects.active = bpy.data.objects[find_object(filename[len(filename)-14 : len(filename)])]
+
+        fileidx = fileidx + 1
+        
         return {'FINISHED'}
 
 def find_object(targetname):
@@ -117,11 +164,12 @@ def register():
     bpy.utils.register_class(GeneralUI)
     bpy.utils.register_class(NextButtom)
     bpy.utils.register_class(PrevButtom)
+    bpy.utils.register_class(OpenButtom)
         
 def unregister():
     bpy.utils.register_class(GeneralUI)
     bpy.utils.register_class(StartButtom)
-    bpy.utils.register_class(PrevButtom)
+    bpy.utils.register_class(OpenButtom)
     pass
 
 if __name__ == "__main__":
